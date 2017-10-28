747672efd91bf52078eea16cde323457d3c3d4f5
Louis Vézina <5130500+morpheus65535@users.noreply.github.com>
2017-10-27 22:22:04 -0400

    Test

--- check_update.py
+++ check_update.py
@@ -3,6 +3,6 @@ from get_general_settings import *
 import git
 
 g = git.cmd.Git(os.path.dirname(__file__))
-g.pull('origin ' + branch + '--dry-run')
+g.pull('origin ' + branch + ' --dry-run')
 
 print g
