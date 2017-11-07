d0ca6db2f7315944b1adcdff3f1a50d4e7173d51
Louis Vézina <5130500+morpheus65535@users.noreply.github.com>
2017-11-07 00:11:42 -0500

    Functionnal update

--- check_update.py
+++ check_update.py
@@ -2,8 +2,10 @@ from get_general_settings import *
 
 import os
 import subprocess
-result =  subprocess.check_output(["git", "pull", '--dry-run', 'origin', branch], stderr=subprocess.STDOUT).split('\n')
 
-if len(result) > 2:
-    subprocess.check_output(["git", "pull", 'origin', branch])
-    os.execlp('python', os.path.join(os.path.dirname(__file__), 'bazarr.py'))
+def check_and_apply_update:
+    result =  subprocess.check_output(["git", "pull", '--dry-run', 'origin', branch], stderr=subprocess.STDOUT).split('\n')
+
+    if result[2] is not '':
+        subprocess.check_output(["git", "pull", 'origin', branch])
+        os.execlp('python', 'python ' + os.path.join(os.path.dirname(__file__), 'bazarr.py'))
--- scheduler.py
+++ scheduler.py
@@ -5,6 +5,7 @@ from get_subtitle import *
 from apscheduler.schedulers.background import BackgroundScheduler
 
 scheduler = BackgroundScheduler()
+scheduler.add_job(update_bazarr, 'interval', hours=6, max_instances=1, coalesce=True, id='update_bazarr', name='Update bazarr from source on Github')
 scheduler.add_job(update_series, 'interval', minutes=1, max_instances=1, coalesce=True, id='update_series', name='Update series list from Sonarr')
 scheduler.add_job(add_new_episodes, 'interval', minutes=1, max_instances=1, coalesce=True, id='add_new_episodes', name='Add new episodes from Sonarr')
 scheduler.add_job(wanted_search_missing_subtitles, 'interval', minutes=15, max_instances=1, coalesce=True, id='wanted_search_missing_subtitles', name='Search for wanted subtitles')
