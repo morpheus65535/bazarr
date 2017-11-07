45f5c0748e6e0c36b964a805238a5dcdc7f911f8
Louis Vézina <5130500+morpheus65535@users.noreply.github.com>
2017-11-07 10:17:35 -0500

    Revert "Update test"
    This reverts commit 61f1fbb4982cc6094237b5d610e82844315ed5d8.

--- check_update.py
+++ check_update.py
@@ -5,10 +5,7 @@ import subprocess
 
 def check_and_apply_update():
     result =  subprocess.check_output(["git", "pull", '--dry-run', 'origin', branch], stderr=subprocess.STDOUT).split('\n')
-    print result
 
     if result[2] is not '':
         subprocess.check_output(["git", "pull", 'origin', branch])
         os.execlp('python', 'python ' + os.path.join(os.path.dirname(__file__), 'bazarr.py'))
-
-check_and_apply_update()
