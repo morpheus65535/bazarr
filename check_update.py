2e3d306e84288989a1eaac130d7c91d2bc0dc085
Louis Vézina <5130500+morpheus65535@users.noreply.github.com>
2017-11-06 23:53:31 -0500

    Update WIP

--- check_update.py
+++ check_update.py
@@ -1,5 +1,9 @@
 from get_general_settings import *
 
-import git
+import os
 import subprocess
-print subprocess.check_output(["git", "pull", '--dry-run', '--quiet', 'origin', branch], stderr=subprocess.STDOUT)
+result =  subprocess.check_output(["git", "pull", '--dry-run', 'origin', branch], stderr=subprocess.STDOUT).split('\n')
+
+if len(result) > 2:
+    subprocess.check_output(["git", "pull", 'origin', branch])
+    os.execlp('python', os.path.join(os.path.dirname(__file__), 'bazarr.py'))
