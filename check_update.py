c0ce5362eaa8a9fdad582dd3b0cb524fd421eae4
Louis Vézina <5130500+morpheus65535@users.noreply.github.com>
2017-11-08 20:57:17 -0500

    Include cwd in update

--- check_update.py
+++ check_update.py
@@ -4,8 +4,8 @@ import os
 import subprocess
 
 def check_and_apply_update():
-    result =  subprocess.check_output(["git", "pull", '--dry-run', 'origin', branch], stderr=subprocess.STDOUT, shell=True).split('\n')
+    result =  subprocess.check_output(["git", "pull", '--dry-run', 'origin', branch], stderr=subprocess.STDOUT, shell=True, cwd=os.path.join(os.path.dirname(__file__)).split('\n')
 
     if result[2] is not '':
-        subprocess.check_output(["git", "pull", 'origin', branch], shell=True)
+        subprocess.check_output(["git", "pull", 'origin', branch], shell=True, cwd=os.path.join(os.path.dirname(__file__))
         os.execlp('python', 'python ' + os.path.join(os.path.dirname(__file__), 'bazarr.py'))
