26b4eb3bb4f9ec41ad8f7baf07f02bc3ccaa1736
Louis Vézina <5130500+morpheus65535@users.noreply.github.com>
2017-11-09 16:23:00 -0500

    Test

--- bazarr.py
+++ bazarr.py
@@ -302,7 +302,7 @@ def save_settings():
 def check_update():
     ref = request.environ['HTTP_REFERER']
 
-    root.info(check_and_apply_update())
+    root.warning(check_and_apply_update())
     
     redirect(ref)
 
