754b05863528b19249e6e8b7b558405259b5f1cf
Louis Vézina <5130500+morpheus65535@users.noreply.github.com>
2017-11-14 10:44:07 -0500

    Test

--- list_subtitles.py
+++ list_subtitles.py
@@ -106,7 +106,7 @@ def full_scan_subtitles():
     c_db.close()
 
     for episode in episodes:
-        store_subtitles(path_replace(episode[0]))
+        store_subtitles(path_replace(episode[0].encode('utf-8')))
 
 def series_scan_subtitles(no):
     conn_db = sqlite3.connect(os.path.join(os.path.dirname(__file__), 'data/db/bazarr.db'))
