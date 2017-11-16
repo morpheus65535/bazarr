77162c1d48f04593d43e1c8bfb9b8085ac2c0831
Louis Vézina <5130500+morpheus65535@users.noreply.github.com>
2017-11-16 13:30:48 -0500

    Test

--- get_subtitle.py
+++ get_subtitle.py
@@ -13,18 +13,18 @@ region.configure('dogpile.cache.dbm', arguments={'filename': os.path.join(os.pat
 
 def download_subtitle(path, language, hi, providers):
     video = scan_video(path)
-    best_subtitles = download_best_subtitles([video], {Language(language)}, providers=providers, hearing_impaired=hi)
-    try:
-        best_subtitle = best_subtitles[video][0]
-    
-        result = save_subtitles(video, [best_subtitle])
-        downloaded_provider = str(result[0]).strip('<>').split(' ')[0][:-8]
-        downloaded_language = pycountry.languages.lookup(str(str(result[0]).strip('<>').split(' ')[2].strip('[]'))).name
-        message = downloaded_language + " subtitles downloaded from " + downloaded_provider + "."
-
-        return message
-    except:
-        return None
+    #best_subtitles = download_best_subtitles([video], {Language(language)}, providers=providers, hearing_impaired=hi)
+    #try:
+    #    best_subtitle = best_subtitles[video][0]
+    #
+    #    result = save_subtitles(video, [best_subtitle])
+    #    downloaded_provider = str(result[0]).strip('<>').split(' ')[0][:-8]
+    #    downloaded_language = pycountry.languages.lookup(str(str(result[0]).strip('<>').split(' ')[2].strip('[]'))).name
+    #    message = downloaded_language + " subtitles downloaded from " + downloaded_provider + "."
+    #
+    #    return message
+    #except:
+    #    return None
 
     del video
     del best_subtitles
