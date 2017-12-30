3a4e646c76be3b6413380b913496a13c49d0b2de
morpheus65535 <5130500+morpheus65535@users.noreply.github.com>
2017-12-30 08:33:36 -0500

    Bug in update process

--- check_update.py
+++ check_update.py
@@ -11,12 +11,12 @@ current_working_directory = os.path.dirname(__file__)
 def check_and_apply_update():
     g = git.cmd.Git(current_working_directory)
     result = g.pull('origin', branch)
-    if result != 'Already up-to-date.':
+    if result.startswith('Already'):
+        logging.info('No new version of Bazarr available.')
+    else:
         #logging.info('Bazarr updated to latest version and need to be restarted.')
         logging.info(result)
         updated()
-    else:
-        logging.info('No new version of Bazarr available.')
 
 def updated():
     conn = sqlite3.connect(os.path.join(os.path.dirname(__file__), 'data/db/bazarr.db'), timeout=30)
