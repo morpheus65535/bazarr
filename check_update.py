7d5c1c235c7a9317d6f9edc4a5942da5a89b3751
morpheus65535 <5130500+morpheus65535@users.noreply.github.com>
2018-01-01 14:35:32 -0500

    Bug correction in updater

--- check_update.py
+++ check_update.py
@@ -15,9 +15,9 @@ def check_and_apply_update():
         logging.info('No new version of Bazarr available.')
     elif result.startswith('Updating'):
         logging.info('Bazarr updated to latest version and need to be restarted.')
+        updated()
     else:
         logging.info(result)
-        updated()
 
 def updated():
     conn = sqlite3.connect(os.path.join(os.path.dirname(__file__), 'data/db/bazarr.db'), timeout=30)
