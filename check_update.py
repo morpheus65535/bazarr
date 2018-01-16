be7c33f2c5ee476190dee369ac5d96809b687612
morpheus65535 <5130500+morpheus65535@users.noreply.github.com>
2018-01-15 23:00:29 -0500

    Included git name and email configuration if not already set to avoid update error.

--- check_update.py
+++ check_update.py
@@ -8,7 +8,23 @@ import git
 
 current_working_directory = os.path.dirname(__file__)
 
+def gitconfig():
+    g = git.Repo.init(current_working_directory)
+    config_read = g.config_reader()
+    config_write = g.config_writer()
+    
+    try:
+        username = config_read.get_value("user", "name")
+    except:
+        config_write.set_value("user", "name", "Bazarr")
+    
+    try:
+        email = config_read.get_value("user", "email")
+    except:
+        config_write.set_value("user", "email", "bazarr@fake.email")
+
 def check_and_apply_update():
+    gitconfig()
     g = git.cmd.Git(current_working_directory)
     result = g.pull('origin', branch)
     if result.startswith('Already'):
