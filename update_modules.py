0a6b82eedd25dcd203b0df2896b903a105be01e6
morpheus65535 <5130500+morpheus65535@users.noreply.github.com>
2018-03-28 12:07:35 -0400

    Update to stop using "import pip" which is an unsupported usage.

--- update_modules.py
+++ update_modules.py
@@ -1,21 +1,28 @@
-import pip
+import subprocess
+from subprocess import check_output
+import logging
+import os
+import sys
 
 try:
-    pip.main(['install', '--user', 'gitpython'])
-except SystemExit as e:
-    pass
+    logging.info('Installing Python modules required for Bazarr...')
 
-try:
-    pip.main(['install', '--user', 'langdetect'])
-except SystemExit as e:
-    pass
+    command = sys.executable + ' -m pip --disable-pip-version-check -q -q install -r ' + os.path.join(os.path.dirname(__file__), 'requirements.txt')
 
-try:
-    pip.main(['install', '--user', 'apprise'])
-except SystemExit as e:
-    pass
+    if os.name == 'nt':
+        codepage = check_output("chcp", shell=True, stderr=subprocess.STDOUT)
+        encoding = codepage.split(':')[-1].strip()
 
-try:
-    pip.main(['install', '--user', 'tzlocal'])
-except SystemExit as e:
+    process = check_output(command, shell=True, stderr=subprocess.STDOUT)
+
+    if os.name == 'nt':
+        process = process.decode(encoding)
+except:
+    logging.error('Unable to install requirements using command line PIP. Is PIP installed and included in system path?')
     pass
+else:
+    if process == "":
+        logging.info('Required Python modules installed if missing.')
+    else:
+        for line in process.splitlines():
+            logging.error(line)
