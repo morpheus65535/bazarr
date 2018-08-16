933e4e413f7923af6f22b67e3ba7b99b74784e30
morpheus65535 <5130500+morpheus65535@users.noreply.github.com>
2018-08-15 22:06:38 -0400

    Fix for #85

--- bazarr.py
+++ bazarr.py
@@ -9,7 +9,7 @@ import os
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
-sys.path.insert(0, os.path.join(config_dir, 'libs/'))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'libs/'))
 
 import sqlite3
 from init_db import *
