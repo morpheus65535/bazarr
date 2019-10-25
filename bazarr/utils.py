5ec8f20ccfde31257d5015318648ff9f93f60b52
morpheus65535 <louis_vezina@hotmail.com>
2019-10-25 09:48:22 -0400

    Update utils.py

--- bazarr/utils.py
+++ bazarr/utils.py
@@ -105,7 +105,7 @@ def get_sonarr_version():
         try:
             sonarr_version = requests.get(sv, timeout=60, verify=False).json()['version']
         except Exception as e:
-            logging.DEBUG('BAZARR cannot get Sonarr version')
+            logging.debug('BAZARR cannot get Sonarr version')
 
     return sonarr_version
 
@@ -119,6 +119,6 @@ def get_radarr_version():
         try:
             radarr_version = requests.get(rv, timeout=60, verify=False).json()['version']
         except Exception as e:
-            logging.DEBUG('BAZARR cannot get Radarr version')
+            logging.debug('BAZARR cannot get Radarr version')
 
     return radarr_version
