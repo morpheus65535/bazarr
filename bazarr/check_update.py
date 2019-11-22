03e98fdb95c32a14419fc5e0f954efcc4bfc1bf5
Louis Vézina <5130500+morpheus65535@users.noreply.github.com>
2019-11-21 21:30:06 -0500

    Fix for restart after update from source on github.

--- bazarr/check_update.py
+++ bazarr/check_update.py
@@ -4,6 +4,7 @@ import logging
 import json
 import requests
 import tarfile
+import sys
 
 from get_args import args
 from config import settings
@@ -294,10 +295,14 @@ def request_json(url, **kwargs):
 def updated(restart=True):
     if settings.general.getboolean('update_restart') and restart:
         try:
-            from main import restart
-            restart()
-        except:
-            logging.info('BAZARR Restart failed, please restart Bazarr manualy')
+            database.close()
+            restart_file = open(os.path.join(args.config_dir, "bazarr.restart"), "w")
+            logging.info('Bazarr is being restarted...')
+            restart_file.write('')
+            restart_file.close()
+            sys.exit(0)
+        except Exception as e:
+            logging.info('BAZARR Restart failed, please restart Bazarr manually')
             updated(restart=False)
     else:
         database.execute("UPDATE system SET updated='1'")
