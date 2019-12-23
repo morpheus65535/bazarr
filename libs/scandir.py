133787cf231f1017e777fba0a19c8787f2720c1d
Louis Vézina <5130500+morpheus65535@users.noreply.github.com>
2019-12-23 12:26:49 -0500

    Fix for #709.

--- libs/scandir.py
+++ libs/scandir.py
@@ -462,6 +462,15 @@ elif sys.platform.startswith(('linux', 'darwin', 'sunos5')) or 'bsd' in sys.plat
             @property
             def path(self):
                 if self._path is None:
+                    # Make sure both attributes are string
+                    try:
+                        self._scandir_path = self._scandir_path.decode(encoding="utf-8")
+                    except (UnicodeDecodeError, AttributeError):
+                        pass
+                    try:
+                        self.name = self.name.decode(encoding="utf-8")
+                    except (UnicodeDecodeError, AttributeError):
+                        pass
                     self._path = join(self._scandir_path, self.name)
                 return self._path
 
