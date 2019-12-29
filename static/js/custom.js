e9d5fa5c80367f194cb06b634e5669a16d87946a
Moravčík, Marian <marian.moravcik@emea.ironmountain.com>
2019-12-29 23:53:48 +0100

    Some cleanup

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,16 +1,10 @@
-/*
-Template Name: Monster Admin
-Author: Themedesigner
-Email: niravjoshi87@gmail.com
-File: js
-*/
 $(function () {
     "use strict";
     $(function () {
         $(".preloader").fadeOut();
     });
     jQuery(document).on('click', '.mega-dropdown', function (e) {
-        e.stopPropagation()
+        e.stopPropagation();
     });
     // ==============================================================
     // This is for the top header part and sidebar part
@@ -74,9 +68,6 @@ $(function () {
     $(".sidebartoggler").on('click', function () {
         //$(".sidebartoggler i").toggleClass("ti-menu");
     });
-    $(".search-box a, .search-box .app-search .srh-btn").on('click', function () {
-        $(".app-search").toggle(200);
-    });
     // ==============================================================
     // Right sidebar options
     // ==============================================================
@@ -107,14 +98,14 @@ $(function () {
     //tooltip
     // ==============================================================
     $(function () {
-        $('[data-toggle="tooltip"]').tooltip()
-    })
+        $('[data-toggle="tooltip"]').tooltip();
+    });
     // ==============================================================
     //Popover
     // ==============================================================
     $(function () {
-        $('[data-toggle="popover"]').popover()
-    })
+        $('[data-toggle="popover"]').popover();
+    });
     // ==============================================================
     // Sidebarmenu
     // ==============================================================
@@ -168,69 +159,4 @@ $(function () {
     // Resize all elements
     // ==============================================================
     $("body").trigger("resize");
-    // ==============================================================
-    // To do list
-    // ==============================================================
-    $(".list-task li label").click(function () {
-        $(this).toggleClass("task-done");
-    });
-
-    // ==============================================================
-    // Login and Recover Password
-    // ==============================================================
-    $('#to-recover').on("click", function () {
-        $("#loginform").slideUp();
-        $("#recoverform").fadeIn();
-    });
-
-    // ==============================================================
-    // Collapsable cards
-    // ==============================================================
-    $('a[data-action="collapse"]').on('click', function (e) {
-        e.preventDefault();
-        $(this).closest('.card').find('[data-action="collapse"] i').toggleClass('ti-minus ti-plus');
-        $(this).closest('.card').children('.card-body').collapse('toggle');
-
-    });
-    // Toggle fullscreen
-    $('a[data-action="expand"]').on('click', function (e) {
-        e.preventDefault();
-        $(this).closest('.card').find('[data-action="expand"] i').toggleClass('mdi-arrow-expand mdi-arrow-compress');
-        $(this).closest('.card').toggleClass('card-fullscreen');
-    });
-
-    // Close Card
-    $('a[data-action="close"]').on('click', function () {
-        $(this).closest('.card').removeClass().slideUp('fast');
-    });
-
-    // ==============================================================
-    // This is for the sparkline charts which is coming in the bradcrumb section
-    // ==============================================================
-    $('#monthchart').sparkline([5, 6, 2, 9, 4, 7, 10, 12], {
-        type: 'bar',
-        height: '35',
-        barWidth: '4',
-        resize: true,
-        barSpacing: '4',
-        barColor: '#1e88e5'
-    });
-    $('#lastmonthchart').sparkline([5, 6, 2, 9, 4, 7, 10, 12], {
-        type: 'bar',
-        height: '35',
-        barWidth: '4',
-        resize: true,
-        barSpacing: '4',
-        barColor: '#7460ee'
-    });
-    var sparkResize;
-
-    // For Custom File Input
-    $('.custom-file-input').on('change', function () {
-        //get the file name
-        var fileName = $(this).val();
-        //replace the "Choose a file" label
-        $(this).next('.custom-file-label').html(fileName);
-    })
-
 });
