cc61d0188f33d33121b95564d9b55d486d5d4544
Louis Vézina <5130500+morpheus65535@users.noreply.github.com>
2020-02-16 10:37:55 -0500

    Fix for Python 2.7 compatibility regression.

--- bazarr.py
+++ bazarr.py
@@ -117,7 +117,10 @@ def start_bazarr(process_registry=ProcessRegistry()):
     script = [sys.executable, "-u", os.path.normcase(os.path.join(dir_name, 'bazarr', 'main.py'))] + sys.argv[1:]
 
     print("Bazarr starting...")
-    ep = subprocess.Popen(script, stdout=None, stderr=None, stdin=subprocess.DEVNULL)
+    if PY3:
+        ep = subprocess.Popen(script, stdout=None, stderr=None, stdin=subprocess.DEVNULL)
+    else:
+        ep = subprocess.Popen(script, stdout=None, stderr=None, stdin=None)
     process_registry.register(ep)
     try:
         ep.wait()
