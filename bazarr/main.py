a3cecd6687f91262cc2157533a3daf34a97d3ba1
Louis Vézina <5130500+morpheus65535@users.noreply.github.com>
2020-04-22 20:07:21 -0400

    WIP

--- bazarr/main.py
+++ bazarr/main.py
@@ -204,235 +204,6 @@ def doRestart():
             os._exit(0)
 
 
-@app.route('/wizard/')
-@login_required
-def wizard():
-    # Get languages list
-    settings_languages = database.execute("SELECT * FROM table_settings_languages ORDER BY name")
-    # Get providers list
-    settings_providers = sorted(provider_manager.names())
-
-    return render_template('wizard.html', bazarr_version=bazarr_version, settings=settings,
-                    settings_languages=settings_languages, settings_providers=settings_providers,
-                    base_url=base_url, ast=ast)
-
-
-@app.route('/save_wizard', methods=['POST'])
-@login_required
-def save_wizard():
-    settings_general_ip = request.form.get('settings_general_ip')
-    settings_general_port = request.form.get('settings_general_port')
-    settings_general_baseurl = request.form.get('settings_general_baseurl')
-    if not settings_general_baseurl.endswith('/'):
-        settings_general_baseurl += '/'
-    settings_general_sourcepath = request.form.getlist('settings_general_sourcepath[]')
-    settings_general_destpath = request.form.getlist('settings_general_destpath[]')
-    settings_general_pathmapping = []
-    settings_general_pathmapping.extend([list(a) for a in zip(settings_general_sourcepath, settings_general_destpath)])
-    settings_general_sourcepath_movie = request.form.getlist('settings_general_sourcepath_movie[]')
-    settings_general_destpath_movie = request.form.getlist('settings_general_destpath_movie[]')
-    settings_general_pathmapping_movie = []
-    settings_general_pathmapping_movie.extend(
-        [list(a) for a in zip(settings_general_sourcepath_movie, settings_general_destpath_movie)])
-    settings_general_single_language = request.form.get('settings_general_single_language')
-    if settings_general_single_language is None:
-        settings_general_single_language = 'False'
-    else:
-        settings_general_single_language = 'True'
-    settings_general_use_sonarr = request.form.get('settings_general_use_sonarr')
-    if settings_general_use_sonarr is None:
-        settings_general_use_sonarr = 'False'
-    else:
-        settings_general_use_sonarr = 'True'
-    settings_general_use_radarr = request.form.get('settings_general_use_radarr')
-    if settings_general_use_radarr is None:
-        settings_general_use_radarr = 'False'
-    else:
-        settings_general_use_radarr = 'True'
-    settings_general_embedded = request.form.get('settings_general_embedded')
-    if settings_general_embedded is None:
-        settings_general_embedded = 'False'
-    else:
-        settings_general_embedded = 'True'
-    settings_subfolder = request.form.get('settings_subfolder')
-    settings_subfolder_custom = request.form.get('settings_subfolder_custom')
-    settings_upgrade_subs = request.form.get('settings_upgrade_subs')
-    if settings_upgrade_subs is None:
-        settings_upgrade_subs = 'False'
-    else:
-        settings_upgrade_subs = 'True'
-    settings_days_to_upgrade_subs = request.form.get('settings_days_to_upgrade_subs')
-    settings_upgrade_manual = request.form.get('settings_upgrade_manual')
-    if settings_upgrade_manual is None:
-        settings_upgrade_manual = 'False'
-    else:
-        settings_upgrade_manual = 'True'
-
-    settings.general.ip = str(settings_general_ip)
-    settings.general.port = str(settings_general_port)
-    settings.general.base_url = str(settings_general_baseurl)
-    settings.general.path_mappings = str(settings_general_pathmapping)
-    settings.general.single_language = str(settings_general_single_language)
-    settings.general.use_sonarr = str(settings_general_use_sonarr)
-    settings.general.use_radarr = str(settings_general_use_radarr)
-    settings.general.path_mappings_movie = str(settings_general_pathmapping_movie)
-    settings.general.subfolder = str(settings_subfolder)
-    settings.general.subfolder_custom = str(settings_subfolder_custom)
-    settings.general.use_embedded_subs = str(settings_general_embedded)
-    settings.general.upgrade_subs = str(settings_upgrade_subs)
-    settings.general.days_to_upgrade_subs = str(settings_days_to_upgrade_subs)
-    settings.general.upgrade_manual = str(settings_upgrade_manual)
-
-    settings_sonarr_ip = request.form.get('settings_sonarr_ip')
-    settings_sonarr_port = request.form.get('settings_sonarr_port')
-    settings_sonarr_baseurl = request.form.get('settings_sonarr_baseurl')
-    settings_sonarr_ssl = request.form.get('settings_sonarr_ssl')
-    if settings_sonarr_ssl is None:
-        settings_sonarr_ssl = 'False'
-    else:
-        settings_sonarr_ssl = 'True'
-    settings_sonarr_apikey = request.form.get('settings_sonarr_apikey')
-    settings_sonarr_only_monitored = request.form.get('settings_sonarr_only_monitored')
-    if settings_sonarr_only_monitored is None:
-        settings_sonarr_only_monitored = 'False'
-    else:
-        settings_sonarr_only_monitored = 'True'
-
-    settings.sonarr.ip = str(settings_sonarr_ip)
-    settings.sonarr.port = str(settings_sonarr_port)
-    settings.sonarr.base_url = str(settings_sonarr_baseurl)
-    settings.sonarr.ssl = str(settings_sonarr_ssl)
-    settings.sonarr.apikey = str(settings_sonarr_apikey)
-    settings.sonarr.only_monitored = str(settings_sonarr_only_monitored)
-
-    settings_radarr_ip = request.form.get('settings_radarr_ip')
-    settings_radarr_port = request.form.get('settings_radarr_port')
-    settings_radarr_baseurl = request.form.get('settings_radarr_baseurl')
-    settings_radarr_ssl = request.form.get('settings_radarr_ssl')
-    if settings_radarr_ssl is None:
-        settings_radarr_ssl = 'False'
-    else:
-        settings_radarr_ssl = 'True'
-    settings_radarr_apikey = request.form.get('settings_radarr_apikey')
-    settings_radarr_only_monitored = request.form.get('settings_radarr_only_monitored')
-    if settings_radarr_only_monitored is None:
-        settings_radarr_only_monitored = 'False'
-    else:
-        settings_radarr_only_monitored = 'True'
-
-    settings.radarr.ip = str(settings_radarr_ip)
-    settings.radarr.port = str(settings_radarr_port)
-    settings.radarr.base_url = str(settings_radarr_baseurl)
-    settings.radarr.ssl = str(settings_radarr_ssl)
-    settings.radarr.apikey = str(settings_radarr_apikey)
-    settings.radarr.only_monitored = str(settings_radarr_only_monitored)
-
-    settings_subliminal_providers = request.form.getlist('settings_subliminal_providers')
-    settings.general.enabled_providers = u'' if not settings_subliminal_providers else ','.join(
-        settings_subliminal_providers)
-
-    settings_addic7ed_random_agents = request.form.get('settings_addic7ed_random_agents')
-    if settings_addic7ed_random_agents is None:
-        settings_addic7ed_random_agents = 'False'
-    else:
-        settings_addic7ed_random_agents = 'True'
-
-    settings_opensubtitles_vip = request.form.get('settings_opensubtitles_vip')
-    if settings_opensubtitles_vip is None:
-        settings_opensubtitles_vip = 'False'
-    else:
-        settings_opensubtitles_vip = 'True'
-
-    settings_opensubtitles_ssl = request.form.get('settings_opensubtitles_ssl')
-    if settings_opensubtitles_ssl is None:
-        settings_opensubtitles_ssl = 'False'
-    else:
-        settings_opensubtitles_ssl = 'True'
-
-    settings_opensubtitles_skip_wrong_fps = request.form.get('settings_opensubtitles_skip_wrong_fps')
-    if settings_opensubtitles_skip_wrong_fps is None:
-        settings_opensubtitles_skip_wrong_fps = 'False'
-    else:
-        settings_opensubtitles_skip_wrong_fps = 'True'
-
-    settings.addic7ed.username = request.form.get('settings_addic7ed_username') or ''
-    settings.addic7ed.password = request.form.get('settings_addic7ed_password') or ''
-    settings.addic7ed.random_agents = str(settings_addic7ed_random_agents) or ''
-    settings.assrt.token = request.form.get('settings_assrt_token') or ''
-    settings.legendasdivx.username = request.form.get('settings_legendasdivx_username') or ''
-    settings.legendasdivx.password = request.form.get('settings_legendasdivx_password') or ''
-    settings.legendastv.username = request.form.get('settings_legendastv_username') or ''
-    settings.legendastv.password = request.form.get('settings_legendastv_password') or ''
-    settings.opensubtitles.username = request.form.get('settings_opensubtitles_username') or ''
-    settings.opensubtitles.password = request.form.get('settings_opensubtitles_password') or ''
-    settings.opensubtitles.vip = str(settings_opensubtitles_vip)
-    settings.opensubtitles.ssl = str(settings_opensubtitles_ssl)
-    settings.opensubtitles.skip_wrong_fps = str(settings_opensubtitles_skip_wrong_fps)
-    settings.xsubs.username = request.form.get('settings_xsubs_username') or ''
-    settings.xsubs.password = request.form.get('settings_xsubs_password') or ''
-    settings.napisy24.username = request.form.get('settings_napisy24_username') or ''
-    settings.napisy24.password = request.form.get('settings_napisy24_password') or ''
-    settings.subscene.username = request.form.get('settings_subscene_username') or ''
-    settings.subscene.password = request.form.get('settings_subscene_password') or ''
-    settings.titlovi.username = request.form.get('settings_titlovi_username') or ''
-    settings.titlovi.password = request.form.get('settings_titlovi_password') or ''
-    settings.betaseries.token = request.form.get('settings_betaseries_token') or ''
-
-    settings_subliminal_languages = request.form.getlist('settings_subliminal_languages')
-    # Disable all languages in DB
-    database.execute("UPDATE table_settings_languages SET enabled=0")
-    for item in settings_subliminal_languages:
-        # Enable each desired language in DB
-        database.execute("UPDATE table_settings_languages SET enabled=1 WHERE code2=?", (item,))
-
-    settings_serie_default_enabled = request.form.get('settings_serie_default_enabled')
-    if settings_serie_default_enabled is None:
-        settings_serie_default_enabled = 'False'
-    else:
-        settings_serie_default_enabled = 'True'
-    settings.general.serie_default_enabled = str(settings_serie_default_enabled)
-
-    settings_serie_default_languages = str(request.form.getlist('settings_serie_default_languages'))
-    if settings_serie_default_languages == "['None']":
-        settings_serie_default_languages = 'None'
-    settings.general.serie_default_language = str(settings_serie_default_languages)
-
-    settings_serie_default_hi = request.form.get('settings_serie_default_hi')
-    if settings_serie_default_hi is None:
-        settings_serie_default_hi = 'False'
-    else:
-        settings_serie_default_hi = 'True'
-    settings.general.serie_default_hi = str(settings_serie_default_hi)
-
-    settings_movie_default_enabled = request.form.get('settings_movie_default_enabled')
-    if settings_movie_default_enabled is None:
-        settings_movie_default_enabled = 'False'
-    else:
-        settings_movie_default_enabled = 'True'
-    settings.general.movie_default_enabled = str(settings_movie_default_enabled)
-
-    settings_movie_default_languages = str(request.form.getlist('settings_movie_default_languages'))
-    if settings_movie_default_languages == "['None']":
-        settings_movie_default_languages = 'None'
-    settings.general.movie_default_language = str(settings_movie_default_languages)
-
-    settings_movie_default_hi = request.form.get('settings_movie_default_hi')
-    if settings_movie_default_hi is None:
-        settings_movie_default_hi = 'False'
-    else:
-        settings_movie_default_hi = 'True'
-    settings.general.movie_default_hi = str(settings_movie_default_hi)
-
-    settings_movie_default_forced = str(request.form.get('settings_movie_default_forced'))
-    settings.general.movie_default_forced = str(settings_movie_default_forced)
-
-    with open(os.path.join(args.config_dir, 'config', 'config.ini'), 'w+') as handle:
-        settings.write(handle)
-
-    configured()
-    return redirect(base_url)
-
-
 @app.route('/emptylog')
 @login_required
 def emptylog():
@@ -484,10 +255,8 @@ def redirect_root():
         return redirect(url_for('series'))
     elif settings.general.getboolean('use_radarr'):
         return redirect(url_for('movies'))
-    elif not settings.general.enabled_providers:
-        return redirect('/wizard')
     else:
-        return redirect('/settings')
+        return redirect(url_for('settingsgeneral'))
 
 
 @app.route('/series/')
@@ -566,6 +335,12 @@ def settingssonarr():
 @login_required
 def settingsradarr():
     return render_template('settingsradarr.html')
+
+
+@app.route('/settings/subtitles/')
+@login_required
+def settingssubtitles():
+    return render_template('settingssubtitles.html')
 
 
 @app.route('/check_update')
@@ -675,7 +450,7 @@ def api_movies_history():
 @app.route('/test_url/<protocol>/<path:url>', methods=['GET'])
 @login_required
 def test_url(protocol, url):
-    url = unquote(url)
+    url = protocol + '://' + unquote(url)
     try:
         result = requests.get(url, allow_redirects=False, verify=False).json()['version']
     except Exception as e:
