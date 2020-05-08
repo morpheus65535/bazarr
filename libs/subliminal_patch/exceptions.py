d9982b36d9538f1882932c8fc28df5892a2b34ca
Bazarr <bazarr@fake.email>
2020-05-08 10:32:12 +0100

    added new IPAddressBlocked exception and cache Legendasdivx login cookies to not throttle site

--- libs/subliminal_patch/exceptions.py
+++ libs/subliminal_patch/exceptions.py
@@ -7,11 +7,13 @@ class TooManyRequests(ProviderError):
     """Exception raised by providers when too many requests are made."""
     pass
 
-
 class APIThrottled(ProviderError):
     pass
-
 
 class ParseResponseError(ProviderError):
     """Exception raised by providers when they are not able to parse the response."""
     pass
+
+class IPAddressBlocked(ProviderError):
+	"""Exception raised when providers block requests from IP Address."""
+	pass
