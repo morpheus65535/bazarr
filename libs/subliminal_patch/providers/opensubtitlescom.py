62dd742c653ae8d1c421d7b5c4d301a2eb7e92bc
Louis Vézina <5130500+morpheus65535@users.noreply.github.com>
2020-07-01 00:28:42 -0400

    WIP

--- libs/subliminal_patch/providers/opensubtitlescom.py
+++ libs/subliminal_patch/providers/opensubtitlescom.py
@@ -1,17 +1,20 @@
 # -*- coding: utf-8 -*-
 import logging
 import os
-import re
 
 from requests import Session, ConnectionError, Timeout, ReadTimeout
 from subzero.language import Language
 
 from babelfish import language_converters
+from subliminal import Episode, Movie
+from subliminal.score import get_equivalent_release_groups
+from subliminal.utils import sanitize_release_group, sanitize
 from subliminal.exceptions import DownloadLimitExceeded, AuthenticationError, ConfigurationError, ServiceUnavailable, \
     ProviderError
 from subliminal_patch.subtitle import Subtitle, guess_matches
 from subliminal.subtitle import fix_line_ending, SUBTITLE_EXTENSIONS
 from subliminal_patch.providers import Provider
+from guessit import guessit
 
 logger = logging.getLogger(__name__)
 
@@ -20,53 +23,65 @@ class OpenSubtitlesComSubtitle(Subtitle):
     provider_name = 'opensubtitlescom'
     hash_verifiable = False
 
-    def __init__(self, language, page_link, download_link, description, title, matches, release_info):
+    def __init__(self, language, page_link, file_id, releases, title, year, season=None, episode=None):
         super(OpenSubtitlesComSubtitle, self).__init__(language, hearing_impaired=False,
                                                        page_link=page_link)
-        self.download_link = download_link
-        self.description = description.lower()
         self.title = title
-        self.release_info = release_info
-        self.found_matches = matches
+        self.year = year
+        self.season = season
+        self.episode = episode
+        self.releases = releases
+        self.release_info = releases
+        self.file_id = file_id
+        self.download_link = None
+        self.matches = None
+
 
     @property
     def id(self):
         return self.download_link
 
     def get_matches(self, video):
-        matches = self.found_matches
+        matches = set()
+
+        # handle movies and series separately
+        if isinstance(video, Episode):
+            # series
+            matches.add('series')
+            # year
+            if video.year == self.year:
+                matches.add('year')
+            # season
+            if video.season == self.season:
+                matches.add('season')
+            # episode
+            if video.episode == self.episode:
+                matches.add('episode')
+        # movie
+        elif isinstance(video, Movie):
+            # title
+            matches.add('title')
+            # year
+            if video.year == self.year:
+                matches.add('year')
+
+        # rest is same for both groups
 
         # release_group
-        if video.release_group and video.release_group.lower() in self.description:
+        if (video.release_group and self.releases and
+                any(r in sanitize_release_group(self.releases)
+                    for r in get_equivalent_release_groups(sanitize_release_group(video.release_group)))):
             matches.add('release_group')
-
         # resolution
-        if video.resolution and video.resolution.lower() in self.description:
+        if video.resolution and self.releases and video.resolution in self.releases.lower():
             matches.add('resolution')
-
         # source
-        if video.source:
-            formats = [video.source.lower()]
-            if formats[0] == "web":
-                formats.append("webdl")
-                formats.append("webrip")
-                formats.append("web ")
-            for frmt in formats:
-                if frmt.lower() in self.description:
-                    matches.add('source')
-                    break
+        if video.source and self.releases and video.source.lower() in self.releases.lower():
+            matches.add('source')
+        # other properties
+        matches |= guess_matches(video, guessit(self.releases))
 
-        # video_codec
-        if video.video_codec:
-            video_codecs = [video.video_codec.lower()]
-            if video_codecs[0] == "H.264":
-                formats.append("x264")
-            elif video_codecs[0] == "H.265":
-                formats.append("x265")
-            for vc in formats:
-                if vc.lower() in self.description:
-                    matches.add('video_codec')
-                    break
+        self.matches = matches
 
         return matches
 
@@ -87,18 +102,19 @@ class OpenSubtitlesComProvider(Provider):
         self.token = None
         self.username = username
         self.password = password
-        self.login()
+
+    def initialize(self):
+        if self.token:
+            return True
+        else:
+            self.login()
+
+        self.session.headers.update({'Authorization': self.token})
 
     def terminate(self):
         self.session.close()
-        self.logout()
 
     def login(self):
-        # If already logged in
-        if self.token:
-            return True
-
-        # Else
         try:
             r = self.session.post(self.server_url + 'login',
                                   json={"username": self.username, "password": self.password},
@@ -118,66 +134,82 @@ class OpenSubtitlesComProvider(Provider):
                 raise AuthenticationError('Login failed: %s' % r.reason)
             else:
                 raise ProviderError('Bad status code: ' + str(r.status_code))
+        finally:
             return False
 
+    def search_titles(self, title, video):
+        """Search for titles matching the `title`.
+
+        For episodes, each season has it own title
+        :param str title: the title to search for.
+        :param obj video: the video object.
+        :return: found title.
+        :rtype: dict
+        """
+        title_id = None
+
+        if isinstance(video, Episode):
+            results = self.session.get(self.server_url + 'search/tv', params={'query': title}, timeout=10)
+        else:
+            results = self.session.get(self.server_url + 'search/movie', params={'query': title}, timeout=10)
+        results.raise_for_status()
+
+        # deserialize results
+        try:
+            results_dict = results.json()['data']
+        except ValueError:
+            logger.debug('Unable to parse returned json')
+        else:
+            # loop over results
+            for result in results_dict:
+                if title.lower() == result['attributes']['title'].lower() and \
+                        video.year == int(result['attributes']['year']):
+                    title_id = result['id']
+                    break
+
+            if title_id:
+                return title_id
+            else:
+                logger.debug('No match found for "%s" and "%d"' % (title, video.year))
+
     def query(self, languages, video):
+        if isinstance(video, Episode):
+            title = video.series
+        else:
+            title = video.title
+
+        title_id = self.search_titles(title, video)
+        lang_strings = [str(lang) for lang in languages]
+        langs = ','.join(lang_strings)
+
         # query the server
         result = None
-        year = (" (%d)" % video.year) if video.year else ""
-        q = "%s%s %dx%02d" % (video.series, year, video.season, video.episode)
-        logger.debug('Searching subtitles "%s"', q)
-
-        res = self.session.get(
-            self.server_url + 'search/query', params={'q': q}, timeout=10)
+        if isinstance(video, Episode):
+            res = self.session.get(self.server_url + 'find/tv', params={'parent_id': title_id, 'languages': langs,
+                                                                        'episode_number': video.episode,
+                                                                        'season_number': video.season}, timeout=10)
+        else:
+            res = self.session.get(self.server_url + 'find/movie', params={'id': title_id, 'languages': langs},
+                                   timeout=10)
         res.raise_for_status()
         result = res.json()
 
         subtitles = []
-        for s in [s for s in result if len(s['episodes'])]:
-            for e in s['episodes']:
-                res = self.session.get(
-                    self.server_url + 'episodes/%d' % e['id'], timeout=10)
-                res.raise_for_status()
-                html = res.text
-                for lang_m in re.finditer(
-                        r"<div class=\"subtitle_language\">(.*?)<\/div>.*?(?=<div class=\"subtitle_language\">|<div id=\"subtitle-actions\">)",
-                        html, re.S):
-                    lang = lang_m.group(1)
-                    language = "es"
-                    if "English" in lang:
-                        language = "en"
-                    logger.debug('Found subtitles in "%s" language.', language)
-
-                    for subt_m in re.finditer(
-                            r"<div class=\"version_name\">(.*?)</div>.*?<a href=\"/(subtitles/\d+/download)\" rel=\"nofollow\">(?:.*?<div class=\"version_comments ?\">.*?</i>(.*?)</p>)?",
-                            lang_m.group(0), re.S):
-                        matches = set()
-                        if video.alternative_series is None:
-                            if video.series.lower() == s['name'].lower():
-                                matches.add('series')
-                        elif s['name'].lower() in [video.series.lower()] + list(
-                                map(lambda name: name.lower(), video.alternative_series)):
-                            matches.add('series')
-                        if video.season == e['season']:
-                            matches.add('season')
-                        if video.episode == e['number']:
-                            matches.add('episode')
-                        if video.title == e['name']:
-                            matches.add('title')
-                        # if video.year is None or ("(%d)" % video.year) in s['name']:
-                        matches.add('year')
-                        subtitles.append(
-                            OpenSubtitlesComSubtitle(
-                                Language.fromietf(language),
-                                self.server_url + 'episodes/%d' % e['id'],
-                                self.server_url + subt_m.group(2),
-                                subt_m.group(1) + (subt_m.group(3) if not subt_m.group(3) is None else ""),
-                                e['name'],
-                                matches,
-                                '%s %dx%d,%s,%s' % (
-                                s['name'], e['season'], e['number'], subt_m.group(1), lang_m.group(1)),
-                            )
+        if len(result['data']):
+            for item in result['data']:
+                if item['type'] == 'subtitle':
+                    subtitle = OpenSubtitlesComSubtitle(
+                            language=Language.fromietf(item['attributes']['language']),
+                            page_link=item['attributes']['url'],
+                            file_id=item['attributes']['files'][0]['id'],
+                            releases=item['attributes']['release'],
+                            title=item['attributes']['feature_details']['movie_name'],
+                            year=video.year,
+                            season=item['attributes']['feature_details']['season_number'] if isinstance(video, Episode) else None,
+                            episode=item['attributes']['feature_details']['episode_number'] if isinstance(video, Episode) else None
                         )
+                    subtitle.get_matches(video)
+                    subtitles.append(subtitle)
 
         return subtitles
 
@@ -186,10 +218,16 @@ class OpenSubtitlesComProvider(Provider):
 
     def download_subtitle(self, subtitle):
         logger.info('Downloading subtitle %r', subtitle)
+
+        headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
+        res = self.session.post(self.server_url + 'download', json={'file_id': subtitle.file_id}, headers=headers,
+                                timeout=10)
+        res.raise_for_status()
+        subtitle.download_link = res.json()['link']
+
         r = self.session.get(subtitle.download_link, timeout=10)
         r.raise_for_status()
 
-        r.encoding = "ISO-8859-1"
         subtitle_content = r.content
 
         if subtitle_content:
