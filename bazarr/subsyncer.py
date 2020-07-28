0448c9d26215b3b4cf4c4b745a54e3bbfcfac6f8
Louis Vézina <5130500+morpheus65535@users.noreply.github.com>
2020-07-27 22:03:52 -0400

    Fix for history logging label of synced subtitles based on unknown language audio track.

--- bazarr/subsyncer.py
+++ bazarr/subsyncer.py
@@ -60,7 +60,8 @@ class SubSyncer:
                         self.reference_stream = "s:{}".format(i)
                         break
             if not self.reference_stream:
-                using_what = "{0} embedded subtitle track".format(language_from_alpha3(embedded_subs['language'].alpha3))
+                using_what = "{0} embedded subtitle track".format(
+                    language_from_alpha3(embedded_subs['language'].alpha3) or 'unknown language audio track')
                 self.reference_stream = "s:0"
         elif 'audio' in data:
             audio_tracks = data['audio']
