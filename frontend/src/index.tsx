3156e8a8e404d13036d3e91bec5f1e770bfda1a2
LASER-Yi <ly0007@yeah.net>
2020-10-18 00:38:10 +0800

    rename App to index in App folder

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import App from "./App/App";
+import App from "./App";
 
 import { Provider } from "react-redux";
 import store from "./redux/store";
