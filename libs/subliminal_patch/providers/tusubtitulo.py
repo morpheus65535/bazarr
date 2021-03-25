a9958ea5ffc054ec5c8a7b45e699bb77626f5870
ngosang <ngosang@hotmail.es>
2021-03-25 22:46:13 +0100

    Fixed tusubtitulo provider. resolves #1332

--- libs/subliminal_patch/providers/tusubtitulo.py
+++ libs/subliminal_patch/providers/tusubtitulo.py
@@ -66,7 +66,7 @@ class TuSubtituloProvider(Provider):
     def initialize(self):
         self.session = Session()
         self.session.headers = {
-            "User-Agent": random.choice(AGENT_LIST),
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36",
             "referer": BASE_URL,
         }
 
