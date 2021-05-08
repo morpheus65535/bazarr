8fcffaf3391a51885601ffd23906855a883634c9
LASER-Yi <ly0007@yeah.net>
2021-05-09 02:17:07 +0800

    Add some interfaces in backend to send message and progress

--- bazarr/event_handler.py
+++ bazarr/event_handler.py
@@ -17,3 +17,15 @@ def event_stream(type, action="update", payload=None):
     except (ValueError, TypeError):
         pass
     socketio.emit("data", {"type": type, "action": action, "payload": payload})
+
+
+def show_message(msg):
+    event_stream(type="message", payload=msg)
+
+
+def show_progress(name, percent):
+    event_stream(type="progress", payload={"name": name, "percent": percent})
+
+
+def hide_progress(name):
+    event_stream(type="progress", action="delete", payload={"name": name, "percent": 0})
