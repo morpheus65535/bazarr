78bea39c41e32bfa2b0620e382697f2081cd2a43
morpheus65535 <louis_vezina@hotmail.com>
2021-09-07 19:39:48 -0400

    WIP

--- frontend/src/Settings/keys.ts
+++ frontend/src/Settings/keys.ts
@@ -2,5 +2,5 @@ export const enabledLanguageKey = "languages-enabled";
 export const languageProfileKey = "languages-profiles";
 export const notificationsKey = "notifications-providers";
 
-export const seriesEnabledKey = "settings-general-use_seris";
+export const seriesEnabledKey = "settings-general-use_series";
 export const moviesEnabledKey = "settings-general-use_movies";
