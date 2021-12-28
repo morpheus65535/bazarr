c05997bd3386bd8a7ace3fa2a8a5b17a456b929d
Vitiko <averroista@protonmail.com>
2021-12-28 15:46:09 -0400

    no log: update fese version

--- libs/fese/__init__.py
+++ libs/fese/__init__.py
@@ -14,7 +14,7 @@ from babelfish import Language
 from babelfish.exceptions import LanguageError
 import pysubs2
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 logger = logging.getLogger(__name__)
 
