8123fc18203c0810f25e4c01b954af0f99755dc5
LASER-Yi <liangyi0007@gmail.com>
2022-01-24 21:54:42 +0800

    Fix Lazy component

--- frontend/src/components/Lazy.tsx
+++ frontend/src/components/Lazy.tsx
@@ -2,7 +2,7 @@ import React, { FunctionComponent, Suspense } from "react";
 import { LoadingIndicator } from ".";
 
 const Lazy: FunctionComponent = ({ children }) => {
-  return <Suspense fallback={LoadingIndicator}>{children}</Suspense>;
+  return <Suspense fallback={<LoadingIndicator />}>{children}</Suspense>;
 };
 
 export default Lazy;
