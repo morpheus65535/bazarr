ea219584e63b2c846e54ccc398ca0b4e0e6a1a9e
morpheus65535 <louis_vezina@hotmail.com>
2022-02-22 22:34:03 -0500

    WIP

--- frontend/src/apis/hooks/system.ts
+++ frontend/src/apis/hooks/system.ts
@@ -105,6 +105,7 @@ export function useRunTask() {
     {
       onSuccess: () => {
         client.invalidateQueries([QueryKeys.System, QueryKeys.Tasks]);
+        client.invalidateQueries([QueryKeys.System, QueryKeys.Backups]);
       },
     }
   );
