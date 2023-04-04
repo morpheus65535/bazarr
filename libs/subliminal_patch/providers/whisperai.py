92899eaa53c08f0533ddfb23e9e885c2316f646e
Alex Yancey <alexyancey3@gmail.com>
2023-04-04 16:58:35 -0700

    Re-factor language to go back and forth easily between Whisper languages and subzero (babelfish) languages. Detect language with Whisper if audio languages are not available. Detect if we need to transcribe or translate based on languages.

--- libs/subliminal_patch/providers/whisperai.py
+++ libs/subliminal_patch/providers/whisperai.py
@@ -10,10 +10,157 @@ from subliminal.exceptions import ConfigurationError
 from subzero.language import Language
 from subliminal.video import Episode, Movie
 
-import ffmpeg
+from babelfish.exceptions import LanguageReverseError
 
+import ffmpeg
+import functools
+
+# These are all the languages Whisper supports.
+# from whisper.tokenizer import LANGUAGES
+
+whisper_languages = {
+    "en": "english",
+    "zh": "chinese",
+    "de": "german",
+    "es": "spanish",
+    "ru": "russian",
+    "ko": "korean",
+    "fr": "french",
+    "ja": "japanese",
+    "pt": "portuguese",
+    "tr": "turkish",
+    "pl": "polish",
+    "ca": "catalan",
+    "nl": "dutch",
+    "ar": "arabic",
+    "sv": "swedish",
+    "it": "italian",
+    "id": "indonesian",
+    "hi": "hindi",
+    "fi": "finnish",
+    "vi": "vietnamese",
+    "he": "hebrew",
+    "uk": "ukrainian",
+    "el": "greek",
+    "ms": "malay",
+    "cs": "czech",
+    "ro": "romanian",
+    "da": "danish",
+    "hu": "hungarian",
+    "ta": "tamil",
+    "no": "norwegian",
+    "th": "thai",
+    "ur": "urdu",
+    "hr": "croatian",
+    "bg": "bulgarian",
+    "lt": "lithuanian",
+    "la": "latin",
+    "mi": "maori",
+    "ml": "malayalam",
+    "cy": "welsh",
+    "sk": "slovak",
+    "te": "telugu",
+    "fa": "persian",
+    "lv": "latvian",
+    "bn": "bengali",
+    "sr": "serbian",
+    "az": "azerbaijani",
+    "sl": "slovenian",
+    "kn": "kannada",
+    "et": "estonian",
+    "mk": "macedonian",
+    "br": "breton",
+    "eu": "basque",
+    "is": "icelandic",
+    "hy": "armenian",
+    "ne": "nepali",
+    "mn": "mongolian",
+    "bs": "bosnian",
+    "kk": "kazakh",
+    "sq": "albanian",
+    "sw": "swahili",
+    "gl": "galician",
+    "mr": "marathi",
+    "pa": "punjabi",
+    "si": "sinhala",
+    "km": "khmer",
+    "sn": "shona",
+    "yo": "yoruba",
+    "so": "somali",
+    "af": "afrikaans",
+    "oc": "occitan",
+    "ka": "georgian",
+    "be": "belarusian",
+    "tg": "tajik",
+    "sd": "sindhi",
+    "gu": "gujarati",
+    "am": "amharic",
+    "yi": "yiddish",
+    "lo": "lao",
+    "uz": "uzbek",
+    "fo": "faroese",
+    "ht": "haitian creole",
+    "ps": "pashto",
+    "tk": "turkmen",
+    "nn": "nynorsk",
+    "mt": "maltese",
+    "sa": "sanskrit",
+    "lb": "luxembourgish",
+    "my": "myanmar",
+    "bo": "tibetan",
+    "tl": "tagalog",
+    "mg": "malagasy",
+    "as": "assamese",
+    "tt": "tatar",
+    "haw": "hawaiian",
+    "ln": "lingala",
+    "ha": "hausa",
+    "ba": "bashkir",
+    "jw": "javanese",
+    "su": "sundanese",
+}
 
 logger = logging.getLogger(__name__)
+
+
+@functools.lru_cache(2)
+def encode_audio_stream(path):
+    # Returns the first audio stream
+    # TODO: pick the best audio stream
+
+    logger.debug("Encoding audio stream to WAV with ffmpeg")
+
+    try:
+        # This launches a subprocess to decode audio while down-mixing and resampling as necessary.
+        # Requires the ffmpeg CLI and `ffmpeg-python` package to be installed.
+        out, _ = (
+            ffmpeg.input(path, threads=0)
+            .output("-", format="s16le", acodec="pcm_s16le", ac=1, ar=16000)
+            .run(cmd=["ffmpeg", "-nostdin"], capture_stdout=True, capture_stderr=True)
+        )
+    except ffmpeg.Error as e:
+        raise RuntimeError(f"Failed to load audio: {e.stderr.decode()}") from e
+
+    logger.debug(f"Finished encoding audio stream in {path} with no errors")
+
+    return out
+
+
+def whisper_get_language(code, name):
+    # Whisper uses an inconsistent mix of alpha2 and alpha3 language codes
+    try:
+        return Language.fromalpha2(code)
+    except LanguageReverseError:
+        return Language.fromname(name)
+
+
+def whisper_get_language_reverse(alpha3):
+    # Returns the whisper language code given an alpha3b language
+    for wl in whisper_languages:
+        lan = whisper_get_language(wl, whisper_languages[wl])
+        if lan.alpha3 == alpha3:
+            return wl
+    raise ValueError
 
 
 class WhisperAISubtitle(Subtitle):
@@ -25,6 +172,8 @@ class WhisperAISubtitle(Subtitle):
         super(WhisperAISubtitle, self).__init__(language)
 
         self.video = video
+        self.task = None
+        self.audio_language = None
 
     @property
     def id(self):
@@ -44,13 +193,11 @@ class WhisperAISubtitle(Subtitle):
 class WhisperAIProvider(Provider):
     '''Whisper AI Provider.'''
 
-    # These are all the languages Whisper supports.
-    # from whisper.tokenizer import LANGUAGES
-    whisper_languages = ['eng', 'chi', 'ger', 'spa', 'rus', 'kor', 'fre', 'jpn', 'por', 'tur', 'pol', 'cat', 'dut', 'ara', 'swe', 'ita', 'ind', 'hin', 'fin', 'vie', 'heb', 'ukr', 'gre', 'may', 'cze', 'rum', 'dan', 'hun', 'tam', 'nor', 'tha', 'urd', 'hrv', 'bul', 'lit', 'lat', 'mao', 'mal', 'wel', 'slo', 'tel', 'per', 'lav', 'ben', 'srp', 'aze', 'slv', 'kan', 'est', 'mac', 'bre', 'baq', 'ice', 'arm', 'nep', 'mon', 'bos', 'kaz', 'alb', 'swa', 'glg', 'mar', 'pan', 'sin', 'khm', 'sna', 'yor', 'som', 'afr', 'oci', 'geo', 'bel', 'tgk', 'snd', 'guj', 'amh', 'yid', 'lao', 'uzb', 'fao', 'hat', 'pus', 'tuk', 'nno', 'mlt', 'san', 'ltz', 'bur', 'tib', 'tgl', 'mlg', 'asm', 'tat', 'haw', 'lin', 'hau', 'bak', 'jav', 'sun']
+    languages = set()
 
-    languages = {
-        Language.fromalpha3b(lang) for lang in whisper_languages
-    }
+    for lan in whisper_languages:
+        languages.update({whisper_get_language(lan, whisper_languages[lan])})
+
     languages.update(set(Language.rebuild(lang, hi=True) for lang in languages))
     languages.update(set(Language.rebuild(lang, forced=True) for lang in languages))
 
@@ -74,8 +221,51 @@ class WhisperAIProvider(Provider):
     def terminate(self):
         self.session.close()
 
+
+    @functools.lru_cache(2048)
+    def detect_language(self, path) -> Language:
+        out = encode_audio_stream(path)
+
+        r = self.session.post(f"{self.endpoint}/detect-language",
+                              params={'encode': 'false'},
+                              files={'audio_file': out},
+                              timeout=self.timeout)
+
+        logger.info(f"Whisper detected language of {path} as {r.json()['detected_language']}")
+
+        return whisper_get_language(r.json()["language_code"], r.json()["detected_language"])
+
     def query(self, language, video):
-        return WhisperAISubtitle(language, video)
+        if language not in self.languages:
+            return None
+
+        sub = WhisperAISubtitle(language, video)
+        sub.task = "transcribe"
+
+        if video.audio_languages:
+            if language.alpha3 in video.audio_languages:
+                sub.audio_language = language.alpha3
+                # TODO: we should tell ffmpeg to only grab this audio stream... for now, let's hope ;)
+            else:
+                sub.task = "translate"
+                sub.audio_language = list(video.audio_languages)[0]
+        else:
+            # We must detect the language manually
+            detected_lang = self.detect_language(video.original_path)
+
+            if detected_lang != language:
+                sub.task = "translate"
+
+            sub.audio_language = detected_lang.alpha3
+
+        if sub.task == "translate":
+            if language.alpha3 != "eng":
+                logger.info(f"Translation only possible from {language} to English")
+                return None
+
+        logger.debug(f"Whisper ({video.original_path}): {sub.audio_language} -> {language.alpha3} [TASK: {sub.task}]")
+
+        return sub
 
     def list_subtitles(self, video, languages):
         subtitles = [self.query(l, video) for l in languages]
@@ -85,23 +275,10 @@ class WhisperAIProvider(Provider):
         # Invoke Whisper through the API. This may take a long time depending on the file.
         # TODO: This loads the entire file into memory, find a good way to stream the file in chunks
 
-        logger.debug("Encoding audio stream to WAV with ffmpeg")
-
-        try:
-            # This launches a subprocess to decode audio while down-mixing and resampling as necessary.
-            # Requires the ffmpeg CLI and `ffmpeg-python` package to be installed.
-            out, _ = (
-                ffmpeg.input(subtitle.video.original_path, threads=0)
-                .output("-", format="s16le", acodec="pcm_s16le", ac=1, ar=16000)
-                .run(cmd=["ffmpeg", "-nostdin"], capture_stdout=True, capture_stderr=True)
-            )
-        except ffmpeg.Error as e:
-            raise RuntimeError(f"Failed to load audio: {e.stderr.decode()}") from e
-
-        logger.debug(f"Finished encoding audio stream in {subtitle.video.original_path} with no errors")
+        out = encode_audio_stream(subtitle.video.original_path)
 
         r = self.session.post(f"{self.endpoint}/asr",
-                              params={'task': 'transcribe', 'output': 'srt', 'encode': 'false'},
+                              params={'task': subtitle.task, 'language': whisper_get_language_reverse(subtitle.audio_language), 'output': 'srt', 'encode': 'false'},
                               files={'audio_file': out},
                               timeout=self.timeout)
 
