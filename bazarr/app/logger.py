549bebcc43102efaace1920bef92cc59032e2e7d
Max R <mxr@users.noreply.github.com>
2024-01-02 20:04:14 -0800

    Updated logging to use ISO 8601 date format

--- bazarr/app/logger.py
+++ bazarr/app/logger.py
@@ -118,7 +118,7 @@ def configure_logging(debug=False):
         fh = TimedRotatingFileHandler(os.path.join(args.config_dir, 'log/bazarr.log'), when="midnight", interval=1,
                                       backupCount=7, delay=True, encoding='utf-8')
     f = FileHandlerFormatter('%(asctime)s|%(levelname)-8s|%(name)-32s|%(message)s|',
-                             '%d/%m/%Y %H:%M:%S')
+                             '%Y-%m-%d %H:%M:%S')
     fh.setFormatter(f)
     fh.setLevel(log_level)
     logger.addHandler(fh)
