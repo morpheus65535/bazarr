5d87b1047536efb42ee45d53a1ea85a0d536157d
JayZed <jzarycky@sympatico.ca>
2024-03-01 14:43:09 -0500

    Fixed subtitles synchronization process when settings values have changed since Bazarr started

--- bazarr/subtitles/indexer/movies.py
+++ bazarr/subtitles/indexer/movies.py
@@ -264,7 +264,10 @@ def list_missing_subtitles_movies(no=None, send_event=True):
         event_stream(type='badges')
 
 
-def movies_full_scan_subtitles(use_cache=settings.radarr.use_ffprobe_cache):
+def movies_full_scan_subtitles(use_cache=None):
+    if use_cache is None:
+        use_cache = settings.radarr.use_ffprobe_cache
+
     movies = database.execute(
         select(TableMovies.path))\
         .all()
