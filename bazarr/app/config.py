6e2417ce4c27e8b8fe26d8ebadca521174b710ee
carrow8993 <derick.roy0@gmail.com>
2024-05-14 19:47:03 -0700

    Cleaned config entry referencing old ignore vars

--- bazarr/app/config.py
+++ bazarr/app/config.py
@@ -578,8 +578,7 @@ def save_settings(settings_items):
         elif value == 'false':
             value = False
 
-        if key in ['settings-general-use_embedded_subs', 'settings-general-ignore_pgs_subs',
-                   'settings-general-ignore_vobsub_subs', 'settings-general-ignore_ass_subs']:
+        if key in ['settings-general-use_embedded_subs', 'settings-general-ignored_subs]:
             use_embedded_subs_changed = True
 
         if key == 'settings-general-default_und_audio_lang':
