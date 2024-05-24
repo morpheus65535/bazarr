5ca733eac0ec43ebd3ca68e867bfd6ef0fb30cc2
morpheus65535 <louis_vezina@hotmail.com>
2024-05-24 13:19:37 -0400

    Reverted to apprise 1.7.6 to fix an issue with the upgrade process first. 1.8.0 will get back in nightly shortly. #2497

--- libs/apprise/decorators/notify.py
+++ libs/apprise/decorators/notify.py
@@ -26,7 +26,7 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-from .base import CustomNotifyPlugin
+from .CustomNotifyPlugin import CustomNotifyPlugin
 
 
 def notify(on, name=None):
