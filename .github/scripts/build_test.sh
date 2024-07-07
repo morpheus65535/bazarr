67a459f23ebdfb0abfa694ed86351a0f9a7f5a99
morpheus65535 <louis_vezina@hotmail.com>
2024-07-06 23:29:26 -0400

    no log: improved CI build test

--- .github/scripts/build_test.sh
+++ .github/scripts/build_test.sh
@@ -7,9 +7,16 @@ sleep 30
 
 if kill -s 0 $PID
 then
-  echo "Bazarr is still running. We'll kill it..."
-  kill $PID
-  exit 0
+  echo "Bazarr is still running. We'll test if UI is working..."
 else
   exit 1
 fi
+
+exitcode=0
+curl -fsSL --retry-all-errors --retry 60 --retry-max-time 120 --max-time 10 "http://127.0.0.1:6767" --output /dev/null || exitcode=$?
+[[ ${exitcode} == 0 ]] && echo "UI is responsive, good news!" || echo "Oops, UI isn't reachable, bad news..."
+
+echo "Let's stop Bazarr before we exit..."
+pkill -INT -P $PID
+
+exit ${exitcode}
