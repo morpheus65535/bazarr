aba4b1b0b6f5a7bd32064442c1e920f94caafc73
ThisIsntTheWay <msblauwal6@gmail.com>
2024-07-15 14:12:15 +0100

    anidb_id = None

--- bazarr/subtitles/refiners/anidb.py
+++ bazarr/subtitles/refiners/anidb.py
@@ -87,7 +87,7 @@ class AniDBClient(object):
         animes.sort(key=lambda a: a.episode_offset)
 
         # Different from Tvdb, Anidb have different ids for the Parts of a season
-        anidb_id = int(animes[0][0].attrib.get('anidbid'))
+        anidb_id = None
         for index, anime_info in enumerate(animes):
             anime, episode_offset = anime_info
 
