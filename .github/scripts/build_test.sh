b0d69ad2fbed8a4fe5b3d0e2d4e24920a6782247
Omar Pakker <Omar007@users.noreply.github.com>
2024-09-17 19:54:19 +0200

    Update CI workflow jobs to execute tests.

--- .github/scripts/build_test.sh
+++ .github/scripts/build_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 "${ROOT_DIRECTORY}"/bazarr.py --no-update &
+python3 bazarr.py --no-update &
 PID=$!
 
 sleep 30
@@ -12,11 +12,11 @@ else
   exit 1
 fi
 
-exitcode=0
-curl -fsSL --retry-all-errors --retry 60 --retry-max-time 120 --max-time 10 "http://127.0.0.1:6767" --output /dev/null || exitcode=$?
-[[ ${exitcode} == 0 ]] && echo "UI is responsive, good news!" || echo "Oops, UI isn't reachable, bad news..."
+curl -fsSL --retry-all-errors --retry 60 --retry-max-time 120 --max-time 10 "http://127.0.0.1:6767" --output /dev/null
+EXITCODE=$?
+[[ $EXITCODE == 0 ]] && echo "UI is responsive, good news!" || echo "Oops, UI isn't reachable, bad news..."
 
 echo "Let's stop Bazarr before we exit..."
 pkill -INT -P $PID
 
-exit ${exitcode}
+exit $EXITCODE
