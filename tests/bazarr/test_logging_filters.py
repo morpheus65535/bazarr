90b44802dc3be1429b5d41eb4ad4603c80f1ce80
morpheus65535 <louis_vezina@hotmail.com>
2024-09-20 07:42:16 -0400

    Fixed logging filter issue.

--- tests/bazarr/test_logging_filters.py
+++ tests/bazarr/test_logging_filters.py
@@ -3,20 +3,13 @@ import logging
 from bazarr.app.logger import UnwantedWaitressMessageFilter
 
 def test_true_for_bazarr():
-  record = logging.makeLogRecord({
-    "level": logging.INFO,
-    "msg": "a message from BAZARR for logging"
-  })
+  record = logging.LogRecord("", logging.INFO, "", 0, "a message from BAZARR for logging", (), None)
   assert UnwantedWaitressMessageFilter().filter(record)
 
 def test_false_below_error():
-  record = logging.makeLogRecord({
-    "level": logging.INFO
-  })
+  record = logging.LogRecord("", logging.INFO, "", 0, "", (), None)
   assert not UnwantedWaitressMessageFilter().filter(record)
 
 def test_true_error_up():
-  record = logging.makeLogRecord({
-    "level": logging.CRITICAL
-  })
+  record = logging.LogRecord("", logging.CRITICAL, "", 0, "", (), None)
   assert UnwantedWaitressMessageFilter().filter(record)
