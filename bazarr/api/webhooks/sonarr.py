35e1ef22bc190c2e5d75304d0fad2edec0d1d94e
aden <aden@hughorse.net>
2025-05-15 21:16:49 +1000

    Parse Sonarr Webhook events
    Added models for Sonarr events and logic to sync the series before
    attempting to find the corresponding episodes in the Bazarr DB.
    Unfortunately it looks like any non-200 responses force the User
    to re-test the webhook from Sonarr before it will work again, so
    we can't provide very useful codes.

--- bazarr/api/webhooks/sonarr.py
+++ bazarr/api/webhooks/sonarr.py
@@ -1,50 +1,82 @@
 # coding=utf-8
+import logging
 
-from flask_restx import Resource, Namespace, reqparse
+from flask_restx import Resource, Namespace, fields
 
 from app.database import TableEpisodes, TableShows, database, select
+from sonarr.sync.series import update_one_series
 from subtitles.mass_download import episode_download_subtitles
 from subtitles.indexer.series import store_subtitles
 from utilities.path_mappings import path_mappings
+
 
 from ..utils import authenticate
 
 
 api_ns_webhooks_sonarr = Namespace('Webhooks Sonarr', description='Webhooks to trigger subtitles search based on '
-                                                                  'Sonarr episode file ID')
+                                                                  'Sonarr webhooks')
 
 
 @api_ns_webhooks_sonarr.route('webhooks/sonarr')
 class WebHooksSonarr(Resource):
-    post_request_parser = reqparse.RequestParser()
-    post_request_parser.add_argument('eventType', type=str, required=False, help='Event type - used when receiving test hooks from Sonarr.')
-    post_request_parser.add_argument('sonarr_episodefile_id', type=int, required=False, help='Episode file ID')
+
+    episode_file_model = api_ns_webhooks_sonarr.model('SonarrEpisodeFile', {
+        'id':       fields.Integer(required=True, description='Episode file ID'),
+    }, strict=False)
+    
+    series_model = api_ns_webhooks_sonarr.model('SonarrSeries', {
+        'id':         fields.Integer(required=True, description='Series ID'),
+    }, strict=False)
+    
+    sonarr_webhook_model = api_ns_webhooks_sonarr.model('SonarrWebhook', {
+        'episodeFiles':   fields.List(fields.Nested(episode_file_model), required=False, description='List of episode files'),
+        'series':         fields.Nested(series_model, required=False, description='Full series details payload'),
+        'eventType':      fields.String(required=True, description='Type of event (e.g. Test)'),
+    }, strict=False)
 
     @authenticate
-    @api_ns_webhooks_sonarr.doc(parser=post_request_parser)
+    @api_ns_webhooks_sonarr.expect(sonarr_webhook_model, validate=True)
     @api_ns_webhooks_sonarr.response(200, 'Success')
     @api_ns_webhooks_sonarr.response(401, 'Not Authenticated')
-    @api_ns_webhooks_sonarr.response(404, 'Episode file ID not provided')
     def post(self):
         """Search for missing subtitles for a specific episode file id"""
-        args = self.post_request_parser.parse_args()
+        args = api_ns_webhooks_sonarr.payload
         event_type = args.get('eventType')
-        episode_file_id = args.get('sonarr_episodefile_id')
+
+        logging.debug('Received Sonarr webhook event: %s', event_type)
+
         if event_type == 'Test':
-            return '', 200
-        elif not episode_file_id:
-            return 'Episode file ID not provided', 404
+            logging.debug('Received test hook, skipping database search.')
+            return 'Received test hook, skipping database search.', 200
 
 
-        sonarrEpisodeId = database.execute(
-            select(TableEpisodes.sonarrEpisodeId, TableEpisodes.path)
-            .select_from(TableEpisodes)
-            .join(TableShows)
-            .where(TableEpisodes.episode_file_id == episode_file_id)) \
-            .first()
+        # Sonarr hooks only differentiate a download starting vs. ending by
+        # the inclusion of episodeFiles in the payload.
+        episode_file_ids = [e.get('id') for e in args.get('episodeFiles', [])]
 
-        if sonarrEpisodeId:
-            store_subtitles(sonarrEpisodeId.path, path_mappings.path_replace(sonarrEpisodeId.path))
-            episode_download_subtitles(no=sonarrEpisodeId.sonarrEpisodeId, send_progress=True)
+        if not episode_file_ids:
+            logging.debug('No episode file IDs found in the webhook request. Nothing to do.')
+            # Sonarr reports the webhook as 'unhealthy' and requires
+            # user interaction if we return anything except 200s.
+            return 'No episode file IDs found in the webhook request. Nothing to do.', 200
 
-        return '', 200
+        # This webhook is often faster than the database update,
+        # so we update the series first if we can.
+        series_id = args.get('series', {}).get('id')
+        if series_id:
+            update_one_series(series_id, 'updated')
+
+        for efid in episode_file_ids:
+            episode = database.execute(
+                select(TableEpisodes.sonarrEpisodeId, TableEpisodes.path)
+                .select_from(TableEpisodes)
+                .join(TableShows)
+                .where(TableEpisodes.episode_file_id == efid)) \
+                .first()
+
+            if episode:
+                store_subtitles(episode.path, path_mappings.path_replace(episode.path))
+                episode_download_subtitles(no=episode.sonarrEpisodeId, send_progress=True)
+            else:
+                logging.debug('No episode found for episode file ID %s, skipping.', efid)
+        return 'Finished processing subtitles.', 200
