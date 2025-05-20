aa26015abb40bcaaed29e9ac3566e1c12feb0cd4
aden <aden@hughorse.net>
2025-05-20 23:03:20 +1000

    Format code

--- bazarr/api/webhooks/radarr.py
+++ bazarr/api/webhooks/radarr.py
@@ -12,48 +12,71 @@ from utilities.path_mappings import path_mappings
 from ..utils import authenticate
 
 
-api_ns_webhooks_radarr = Namespace('Webhooks Radarr', description='Webhooks to trigger subtitles search based on '
-                                                                  'Radarr webhooks')
+api_ns_webhooks_radarr = Namespace(
+    "Webhooks Radarr",
+    description="Webhooks to trigger subtitles search based on Radarr webhooks",
+)
 
 
-@api_ns_webhooks_radarr.route('webhooks/radarr')
+@api_ns_webhooks_radarr.route("webhooks/radarr")
 class WebHooksRadarr(Resource):
+    movie_model = api_ns_webhooks_radarr.model(
+        "RadarrMovie",
+        {
+            "id": fields.Integer(required=True, description="Movie ID"),
+        },
+        strict=False,
+    )
 
-    movie_model = api_ns_webhooks_radarr.model('RadarrMovie', {
-        'id': fields.Integer(required=True, description='Movie ID'),
-    }, strict=False)
-        
+    movie_file_model = api_ns_webhooks_radarr.model(
+        "RadarrMovieFile",
+        {
+            "id": fields.Integer(required=True, description="Movie file ID"),
+        },
+        strict=False,
+    )
 
-    movie_file_model = api_ns_webhooks_radarr.model('RadarrMovieFile', {
-        'id': fields.Integer(required=True, description='Movie file ID'),
-    }, strict=False)
-
-    radarr_webhook_model = api_ns_webhooks_radarr.model('RadarrWebhook', {
-        'eventType': fields.String(required=True, description='Type of Radarr event (e.g. MovieAdded, Test, etc)'),
-        'movieFile': fields.Nested(movie_file_model, required=False, description='Radarr movie file payload. Required for anything other than test hooks'),
-        'movie': fields.Nested(movie_model, required=False, description='Radarr movie payload. Can be used to sync movies from Radarr if not found in Bazarr'),
-    }, strict=False)
+    radarr_webhook_model = api_ns_webhooks_radarr.model(
+        "RadarrWebhook",
+        {
+            "eventType": fields.String(
+                required=True,
+                description="Type of Radarr event (e.g. MovieAdded, Test, etc)",
+            ),
+            "movieFile": fields.Nested(
+                movie_file_model,
+                required=False,
+                description="Radarr movie file payload. Required for anything other than test hooks",
+            ),
+            "movie": fields.Nested(
+                movie_model,
+                required=False,
+                description="Radarr movie payload. Can be used to sync movies from Radarr if not found in Bazarr",
+            ),
+        },
+        strict=False,
+    )
 
     @authenticate
     @api_ns_webhooks_radarr.expect(radarr_webhook_model, validate=True)
-    @api_ns_webhooks_radarr.response(200, 'Success')
-    @api_ns_webhooks_radarr.response(401, 'Not Authenticated')
+    @api_ns_webhooks_radarr.response(200, "Success")
+    @api_ns_webhooks_radarr.response(401, "Not Authenticated")
     def post(self):
         """Search for missing subtitles based on Radarr webhooks"""
         args = api_ns_webhooks_radarr.payload
-        event_type = args.get('eventType')
+        event_type = args.get("eventType")
 
-        logging.debug(f'Received Radarr webhook event: {event_type}')
+        logging.debug(f"Received Radarr webhook event: {event_type}")
 
-        if event_type == 'Test':
-            message = 'Received test hook, skipping database search.'
+        if event_type == "Test":
+            message = "Received test hook, skipping database search."
             logging.debug(message)
             return message, 200
 
-        movie_file_id = args.get('movieFile', {}).get('id')
+        movie_file_id = args.get("movieFile", {}).get("id")
 
         if not movie_file_id:
-            message = 'No movie file ID found in the webhook request. Nothing to do.'
+            message = "No movie file ID found in the webhook request. Nothing to do."
             logging.debug(message)
             # Radarr reports the webhook as 'unhealthy' and requires
             # user interaction if we return anything except 200s.
@@ -61,21 +84,27 @@ class WebHooksRadarr(Resource):
 
         # This webhook is often faster than the database update,
         # so we update the movie first if we can.
-        radarr_id = args.get('movie', {}).get('id')
-            
-        q = select(TableMovies.radarrId, TableMovies.path).where(TableMovies.movie_file_id == movie_file_id).first()
+        radarr_id = args.get("movie", {}).get("id")
+
+        q = (
+            select(TableMovies.radarrId, TableMovies.path)
+            .where(TableMovies.movie_file_id == movie_file_id)
+            .first()
+        )
 
         movie = database.execute(q)
         if not movie and radarr_id:
-            logging.debug(f'No movie matching file ID {movie_file_id} found in the database. Attempting to sync from Radarr.')
-            update_one_movie(radarr_id, 'updated')
+            logging.debug(
+                f"No movie matching file ID {movie_file_id} found in the database. Attempting to sync from Radarr."
+            )
+            update_one_movie(radarr_id, "updated")
             movie = database.execute(q)
         if not movie:
-            message = f'No movie matching file ID {movie_file_id} found in the database. Nothing to do.'
+            message = f"No movie matching file ID {movie_file_id} found in the database. Nothing to do."
             logging.debug(message)
             return message, 200
-        
+
         store_subtitles_movie(movie.path, path_mappings.path_replace_movie(movie.path))
         movies_download_subtitles(no=movie.radarrId)
 
-        return 'Finished processing subtitles.', 200
+        return "Finished processing subtitles.", 200
--- bazarr/api/webhooks/sonarr.py
+++ bazarr/api/webhooks/sonarr.py
@@ -13,77 +13,109 @@ from utilities.path_mappings import path_mappings
 from ..utils import authenticate
 
 
-api_ns_webhooks_sonarr = Namespace('Webhooks Sonarr', description='Webhooks to trigger subtitles search based on '
-                                                                  'Sonarr webhooks')
+api_ns_webhooks_sonarr = Namespace(
+    "Webhooks Sonarr",
+    description="Webhooks to trigger subtitles search based on Sonarr webhooks",
+)
 
 
-@api_ns_webhooks_sonarr.route('webhooks/sonarr')
+@api_ns_webhooks_sonarr.route("webhooks/sonarr")
 class WebHooksSonarr(Resource):
+    episode_model = api_ns_webhooks_sonarr.model(
+        "SonarrEpisode",
+        {
+            "id": fields.Integer(required=True, description="Episode ID"),
+        },
+        strict=False,
+    )
 
-    episode_model = api_ns_webhooks_sonarr.model('SonarrEpisode', {
-        'id': fields.Integer(required=True, description='Episode ID'),
-    }, strict=False)
+    episode_file_model = api_ns_webhooks_sonarr.model(
+        "SonarrEpisodeFile",
+        {
+            "id": fields.Integer(required=True, description="Episode file ID"),
+        },
+        strict=False,
+    )
 
-    episode_file_model = api_ns_webhooks_sonarr.model('SonarrEpisodeFile', {
-        'id': fields.Integer(required=True, description='Episode file ID'),
-    }, strict=False)
-    
-    sonarr_webhook_model = api_ns_webhooks_sonarr.model('SonarrWebhook', {
-        'episodes': fields.List(fields.Nested(episode_model), required=False, description='List of episodes. Can be used to sync episodes from Sonarr if not found in Bazarr.'),
-        'episodeFiles': fields.List(fields.Nested(episode_file_model), required=False, description='List of episode files; required for anything other than test hooks'),
-        'eventType': fields.String(required=True, description='Type of Sonarr event (e.g. Test, Download, etc.)'),
-    }, strict=False)
+    sonarr_webhook_model = api_ns_webhooks_sonarr.model(
+        "SonarrWebhook",
+        {
+            "episodes": fields.List(
+                fields.Nested(episode_model),
+                required=False,
+                description="List of episodes. Can be used to sync episodes from Sonarr if not found in Bazarr.",
+            ),
+            "episodeFiles": fields.List(
+                fields.Nested(episode_file_model),
+                required=False,
+                description="List of episode files; required for anything other than test hooks",
+            ),
+            "eventType": fields.String(
+                required=True,
+                description="Type of Sonarr event (e.g. Test, Download, etc.)",
+            ),
+        },
+        strict=False,
+    )
 
     @authenticate
     @api_ns_webhooks_sonarr.expect(sonarr_webhook_model, validate=True)
-    @api_ns_webhooks_sonarr.response(200, 'Success')
-    @api_ns_webhooks_sonarr.response(401, 'Not Authenticated')
+    @api_ns_webhooks_sonarr.response(200, "Success")
+    @api_ns_webhooks_sonarr.response(401, "Not Authenticated")
     def post(self):
         """Search for missing subtitles based on Sonarr webhooks"""
         args = api_ns_webhooks_sonarr.payload
-        event_type = args.get('eventType')
+        event_type = args.get("eventType")
 
-        logging.debug(f'Received Sonarr webhook event: {event_type}')
+        logging.debug(f"Received Sonarr webhook event: {event_type}")
 
-        if event_type == 'Test':
-            message = 'Received test hook, skipping database search.'
+        if event_type == "Test":
+            message = "Received test hook, skipping database search."
             logging.debug(message)
             return message, 200
 
-
         # Sonarr hooks only differentiate a download starting vs. ending by
         # the inclusion of episodeFiles in the payload.
-        sonarr_episode_file_ids = [e.get('id') for e in args.get('episodeFiles', [])]
+        sonarr_episode_file_ids = [e.get("id") for e in args.get("episodeFiles", [])]
 
         if not sonarr_episode_file_ids:
-            message = 'No episode file IDs found in the webhook request. Nothing to do.'
+            message = "No episode file IDs found in the webhook request. Nothing to do."
             logging.debug(message)
             # Sonarr reports the webhook as 'unhealthy' and requires
             # user interaction if we return anything except 200s.
             return message, 200
 
-        sonarr_episode_ids = [e.get('id') for e in args.get('episodes', [])]
+        sonarr_episode_ids = [e.get("id") for e in args.get("episodes", [])]
 
         if len(sonarr_episode_ids) != len(sonarr_episode_file_ids):
-            logging.debug('Episode IDs and episode file IDs are different lengths, ignoring episode IDs.')
+            logging.debug(
+                "Episode IDs and episode file IDs are different lengths, ignoring episode IDs."
+            )
             sonarr_episode_ids = []
-            
+
         for i, efid in enumerate(sonarr_episode_file_ids):
-            q = (select(TableEpisodes.sonarrEpisodeId, TableEpisodes.path)
+            q = (
+                select(TableEpisodes.sonarrEpisodeId, TableEpisodes.path)
                 .select_from(TableEpisodes)
                 .join(TableShows)
-                .where(TableEpisodes.episode_file_id == efid)) 
+                .where(TableEpisodes.episode_file_id == efid)
+            )
 
             episode = database.execute(q).first()
             if not episode and sonarr_episode_ids:
-                logging.debug('No episode found for episode file ID %s, attempting to sync from Sonarr.', efid)
+                logging.debug(
+                    "No episode found for episode file ID %s, attempting to sync from Sonarr.",
+                    efid,
+                )
                 sync_one_episode(sonarr_episode_ids[i])
                 episode = database.execute(q).first()
             if not episode:
-                logging.debug('No episode found for episode file ID %s, skipping.', efid)
+                logging.debug(
+                    "No episode found for episode file ID %s, skipping.", efid
+                )
                 continue
 
             store_subtitles(episode.path, path_mappings.path_replace(episode.path))
             episode_download_subtitles(no=episode.sonarrEpisodeId, send_progress=True)
 
-        return 'Finished processing subtitles.', 200
+        return "Finished processing subtitles.", 200
