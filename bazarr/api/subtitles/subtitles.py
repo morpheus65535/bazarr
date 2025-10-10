284c4116162f3cccf40189579c7456f6d031c889
Rowan <rowanfuchs@gmail.com>
2025-10-10 16:21:17 +0200

    improved subtitle translation by retrieving source language from the database

--- bazarr/api/subtitles/subtitles.py
+++ bazarr/api/subtitles/subtitles.py
@@ -15,7 +15,7 @@ from subtitles.tools.translate.main import translate_subtitles_file
 from subtitles.tools.mods import subtitles_apply_mods
 from subtitles.indexer.series import store_subtitles
 from subtitles.indexer.movies import store_subtitles_movie
-from app.config import settings, empty_values
+from app.config import settings, empty_values, get_array_from
 from app.event_handler import event_stream
 
 from ..utils import authenticate
@@ -121,7 +121,7 @@ class Subtitles(Resource):
 
         if media_type == 'episode':
             metadata = database.execute(
-                select(TableEpisodes.path, TableEpisodes.sonarrSeriesId)
+                select(TableEpisodes.path, TableEpisodes.sonarrSeriesId, TableEpisodes.subtitles)
                 .where(TableEpisodes.sonarrEpisodeId == id)) \
                 .first()
 
@@ -131,7 +131,7 @@ class Subtitles(Resource):
             video_path = path_mappings.path_replace(metadata.path)
         else:
             metadata = database.execute(
-                select(TableMovies.path)
+                select(TableMovies.path, TableMovies.subtitles)
                 .where(TableMovies.radarrId == id))\
                 .first()
 
@@ -168,17 +168,32 @@ class Subtitles(Resource):
                 del subsync
                 gc.collect()
         elif action == 'translate':
-            from_language = subtitles_lang_from_filename(subtitles_path)
             dest_language = language
-            try:
-                translate_subtitles_file(video_path=video_path, source_srt_file=subtitles_path,
-                                         from_lang=from_language, to_lang=dest_language, forced=forced, hi=hi,
-                                         media_type="series" if media_type == "episode" else "movies",
-                                         sonarr_series_id=metadata.sonarrSeriesId if media_type == "episode" else None,
-                                         sonarr_episode_id=id,
-                                         radarr_id=id)
-            except OSError:
-                return 'Unable to edit subtitles file. Check logs.', 409
+            from_language = None
+
+            if metadata.subtitles:
+                subtitles_list = get_array_from(metadata.subtitles)
+                subtitles_filename = os.path.basename(subtitles_path)
+
+                for subtitle_entry in subtitles_list:
+                    if len(subtitle_entry) >= 2:
+                        db_subtitle_filename = os.path.basename(subtitle_entry[1])
+                        if db_subtitle_filename == subtitles_filename:
+                            from_language = subtitle_entry[0]
+                            break
+
+                if not from_language:
+                    from_language = subtitles_lang_from_filename(subtitles_path)
+
+                try:
+                    translate_subtitles_file(video_path=video_path, source_srt_file=subtitles_path,
+                                             from_lang=from_language, to_lang=dest_language, forced=forced, hi=hi,
+                                             media_type="series" if media_type == "episode" else "movies",
+                                             sonarr_series_id=metadata.sonarrSeriesId if media_type == "episode" else None,
+                                             sonarr_episode_id=id,
+                                             radarr_id=id)
+                except OSError:
+                    return 'Unable to edit subtitles file. Check logs.', 409
         else:
             use_original_format = True if args.get('original_format') == 'true' else False
             try:
