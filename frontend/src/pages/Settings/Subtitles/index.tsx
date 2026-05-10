7e9e53dd8f193a6d7eabf1f95201eaea70e65675
JayZed <Jzarycky@sympatico.ca>
2026-05-10 15:40:16 -0400

    Reorganized Audio Synchronization Settings
    - made order of options more logical
    - added detailed background descriptions to help users make their choices
    - reworded options for clarity

--- frontend/src/pages/Settings/Subtitles/index.tsx
+++ frontend/src/pages/Settings/Subtitles/index.tsx
@@ -23,6 +23,7 @@ import {
   colorOptions,
   embeddedSubtitlesParserOption,
   folderOptions,
+  forceAudioOption,
   hiExtensionOptions,
   providerOptions,
   syncMaxOffsetSecondsOptions,
@@ -416,60 +417,24 @@ const SettingsSubtitlesView: FunctionComponent = () => {
           playback devices.
         </Message>
       </Section>
-      <Section header="Audio Synchronization / Alignment">
+      <Section header="Audio Synchronization">
         <Check
-          label="Always use Audio Track as Reference for Syncing"
-          settingKey="settings-subsync-force_audio"
-        ></Check>
-        <Message>
-          Use the audio track as reference for syncing, instead of the embedded
-          subtitle.
-        </Message>
-        <Check
-          label="Do Not Fix Framerate Mismatch"
-          settingKey="settings-subsync-no_fix_framerate"
-        ></Check>
-        <Message>
-          If specified, subsync will not attempt to correct a framerate mismatch
-          between reference and subtitles.
-        </Message>
-        <Check
-          label="Golden-Section Search"
-          settingKey="settings-subsync-gss"
-        ></Check>
-        <Message>
-          If specified, use golden-section search to try to find the optimal
-          framerate ratio between video and subtitles.
-        </Message>
-        <Selector
-          label="Max Offset Seconds"
-          options={syncMaxOffsetSecondsOptions}
-          settingKey="settings-subsync-max_offset_seconds"
-          defaultValue={60}
-        ></Selector>
-        <Message>
-          The max allowed offset seconds for any subtitle segment.
-        </Message>
-        <Check
-          label="Automatic Subtitles Audio Synchronization"
+          label="Enable Automatic Subtitles Audio Synchronization"
           settingKey="settings-subsync-use_subsync"
         ></Check>
         <Message>
-          Enable automatic audio synchronization after downloading subtitles.
+          Enable automatic audio synchronization after downloading subtitles for
+          series and movies based on selections below.
         </Message>
-        <CollapseBox indent settingKey="settings-subsync-use_subsync">
-          <MultiSelector
-            placeholder="Select providers..."
-            label="Do not sync subtitles downloaded from those providers"
-            clearable
-            options={providerOptions}
-            settingKey="settings-subsync-checker-blacklisted_providers"
-          ></MultiSelector>
-          <Check label="Debug" settingKey="settings-subsync-debug"></Check>
+        <CollapseBox settingKey="settings-subsync-use_subsync">
           <Message>
-            Do not actually sync the subtitles but generate a .tar.gz file to be
-            able to open an issue for ffsubsync. This file will reside alongside
-            the media file.
+            This feature uses ffsubsync, which can provide better
+            synchronization results than traditional methods, especially for
+            subtitles that are significantly out of sync. However, it may also
+            increase the time it takes to process subtitles. If you have a lot
+            of subtitles that need synchronization or if you are on a
+            low-powered device, you may want to leave this option disabled and
+            synchronize subtitles manually when needed.
           </Message>
           <Check
             label="Series Score Threshold For Audio Sync"
@@ -507,6 +472,67 @@ const SettingsSubtitlesView: FunctionComponent = () => {
               this value will be automatically synchronized.
             </Message>
           </CollapseBox>
+          <MultiSelector
+            placeholder="Select providers..."
+            label="Providers to Exclude from Automatic Synchronization"
+            clearable
+            options={providerOptions}
+            settingKey="settings-subsync-checker-blacklisted_providers"
+          ></MultiSelector>
+          <Message>
+            Subtitles downloaded from the providers listed above will not be
+            automatically synchronized.
+          </Message>
+          <Section header="Advanced FFsubsync Options">
+            <Selector
+              label="Synchronization Reference"
+              options={forceAudioOption}
+              settingKey="settings-subsync-force_audio"
+            ></Selector>
+            <Message>
+              Choose whether to use the audio track or the embedded subtitle as
+              the reference for synchronization. Using the audio track can
+              provide better results, especially when the embedded subtitles are
+              not properly synced or have a different framerate than the video.
+              However, it may also increase the synchronization time, as
+              analyzing the audio track is more resource-intensive than
+              analyzing the embedded subtitles.
+            </Message>
+            <Check
+              label="Do Not Fix Framerate Mismatch"
+              settingKey="settings-subsync-no_fix_framerate"
+            ></Check>
+            <Message>
+              If specified, subsync will not attempt to correct a framerate
+              mismatch between reference and subtitles.
+            </Message>
+            <Check
+              label="Golden-Section Search"
+              settingKey="settings-subsync-gss"
+            ></Check>
+            <Message>
+              If specified, use golden-section search to try to find the optimal
+              framerate ratio between video and subtitles.
+            </Message>
+            <Selector
+              label="Max Offset Seconds"
+              options={syncMaxOffsetSecondsOptions}
+              settingKey="settings-subsync-max_offset_seconds"
+              defaultValue={60}
+            ></Selector>
+            <Message>
+              The max allowed offset seconds for any subtitle segment.
+            </Message>
+            <Check
+              label="Generate Debug File Instead of Synchronizing"
+              settingKey="settings-subsync-debug"
+            ></Check>
+            <Message>
+              Do not actually synchronize the subtitles but generate a .tar.gz
+              file to be able to open an issue for ffsubsync. This file will
+              reside alongside the media file.
+            </Message>
+          </Section>
         </CollapseBox>
       </Section>
       <Section header="Custom Post-Processing">
