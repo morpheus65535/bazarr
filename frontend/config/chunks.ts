344632243a80e57538645a14f5f37a4a0f38f80e
Anderson Shindy Oki <anderson.vs.oki@gmail.com>
2026-06-29 09:29:12 +0900

    chore: Bump vite to v8

--- frontend/config/chunks.ts
+++ frontend/config/chunks.ts
@@ -1,54 +1,58 @@
 // eslint-disable-next-line no-restricted-imports
 import { dependencies } from "../package.json";
 
-const vendors = ["react", "react-router", "react-dom"];
+const groups: Record<string, string[]> = {
+  vendors: ["react", "react-router", "react-dom"],
+  ui: [
+    "@mantine/core",
+    "@mantine/hooks",
+    "@mantine/form",
+    "@mantine/modals",
+    "@mantine/notifications",
+    "@mantine/dropzone",
+  ],
+  query: [
+    "@tanstack/react-query",
+    "@tanstack/react-query-devtools",
+    "@tanstack/react-table",
+  ],
+  charts: [
+    "recharts",
+    "d3-array",
+    "d3-interpolate",
+    "d3-scale",
+    "d3-shape",
+    "d3-time",
+  ],
+  utils: ["axios", "socket.io-client", "lodash", "clsx"],
+};
 
-const ui = [
-  "@mantine/core",
-  "@mantine/hooks",
-  "@mantine/form",
-  "@mantine/modals",
-  "@mantine/notifications",
-  "@mantine/dropzone",
-];
+function buildLookup(): Record<string, string> {
+  const lookup: Record<string, string> = {};
+  const excludeList: string[] = [];
 
-const query = [
-  "@tanstack/react-query",
-  "@tanstack/react-query-devtools",
-  "@tanstack/react-table",
-];
-
-const charts = [
-  "recharts",
-  "d3-array",
-  "d3-interpolate",
-  "d3-scale",
-  "d3-shape",
-  "d3-time",
-];
-
-const utils = ["axios", "socket.io-client", "lodash", "clsx"];
-
-function renderChunks() {
-  const chunks: Record<string, string[]> = {};
-  const excludeList = [...vendors, ...ui, ...query, ...charts, ...utils];
-
-  for (const key in dependencies) {
-    if (!excludeList.includes(key)) {
-      chunks[key] = [key];
+  for (const [group, packages] of Object.entries(groups)) {
+    for (const pkg of packages) {
+      lookup[pkg] = group;
+      excludeList.push(pkg);
     }
   }
 
-  return chunks;
+  for (const key in dependencies) {
+    if (!excludeList.includes(key)) {
+      lookup[key] = key;
+    }
+  }
+
+  return lookup;
 }
 
-const chunks = {
-  vendors,
-  ui,
-  query,
-  charts,
-  utils,
-  ...renderChunks(),
-};
+const lookup = buildLookup();
 
-export default chunks;
+export default function manualChunks(id: string): string | undefined {
+  for (const [pkg, group] of Object.entries(lookup)) {
+    if (id.includes(`/node_modules/${pkg}/`)) {
+      return group;
+    }
+  }
+}
