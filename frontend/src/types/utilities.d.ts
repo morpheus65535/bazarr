e5ee8dd37c4c3d2072f67952b800f528d397332d
Anderson Shindy Oki <anderson.vs.oki@gmail.com>
2026-07-15 13:46:28 +0900

    refactor nested ternary

--- frontend/src/types/utilities.d.ts
+++ frontend/src/types/utilities.d.ts
@@ -8,26 +8,17 @@ type CamelCase<S extends string> = S extends `${infer Head}_${infer Tail}`
   ? `${Head}${CamelCase<Capitalize<Tail>>}`
   : S;
 
+type CamelCaseLeaf =
+  Date | File | Blob | RegExp | ((...args: unknown[]) => unknown);
+
 type CamelCaseKeys<T> =
   T extends ReadonlyArray<infer U>
     ? CamelCaseKeys<U>[]
-    : T extends Date
+    : T extends CamelCaseLeaf
       ? T
-      : T extends File
-        ? T
-        : T extends Blob
-          ? T
-          : T extends RegExp
-            ? T
-            : T extends (...args: unknown[]) => unknown
-              ? T
-              : T extends object
-                ? {
-                    [K in keyof T as CamelCase<K & string>]: CamelCaseKeys<
-                      T[K]
-                    >;
-                  }
-                : T;
+      : T extends object
+        ? { [K in keyof T as CamelCase<K & string>]: CamelCaseKeys<T[K]> }
+        : T;
 
 type LooseObject = {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
